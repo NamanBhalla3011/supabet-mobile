36dd78ea1fe222411514adb60079296373dc2885
Naman Bhalla <68887828+NamanBhalla3011@users.noreply.github.com>
2025-05-16 16:49:57 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
+//Navigation Menu
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+  document.getElementById("myNav").style.width = "100%";
+}
 
-  function openCasino() {
-    document.getElementById("casino").style.width = "100%";
-  }
-  
-  function closeCasino() {
-    document.getElementById("casino").style.width = "0%";
-  }
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+// Casino Menu
+function openCasino() {
+  document.getElementById("casino").style.width = "100%";
+}
+
+function closeCasino() {
+  document.getElementById("casino").style.width = "0%";
+}
+// Iframe
+function openIFrame() {
+  document.getElementById("iframeOverlay").style.display = "flex";
+}
+
+function closeIFrame() {
+  document.getElementById("iframeOverlay").style.display = "none";
+}
